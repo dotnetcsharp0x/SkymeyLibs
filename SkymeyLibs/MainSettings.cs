cf3e7bf26428e445aa1e229da579ca573ae66aa3
dotnetcsharp0x <dsotnikov1996@gmail.com>
2024-08-29 07:30:23 +0300

    configure appsettings like params

---
+++ SkymeyLibs/MainSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkymeyLibs
+{
+    public class MainSettings
+    {
+        public MongoDatabase MongoDatabase { get; set; }
+    }
+    public class MongoDatabase
+    {
+        public string DBName { get; set; }
+        public string DBServer { get; set; }
+    }
+}
